d3f9dad7ce5fb2687f03671f56960c779eed7901
Denis <DUlyanov@srt-Ilc.ru>
2025-11-25 12:15:54 +0700

    Initial Commit

---
+++ UIViewRepresentableWithDynamicHeight_iOS_16/UIViewRepresentableWithDynamicHeight_iOS_16App.swift
@@ -0,0 +1,17 @@
+//
+//  UIViewRepresentableWithDynamicHeight_iOS_16App.swift
+//  UIViewRepresentableWithDynamicHeight_iOS_16
+//
+//  Created by FIX PRICE on 25/11/25.
+//
+
+import SwiftUI
+
+@main
+struct UIViewRepresentableWithDynamicHeight_iOS_16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
